c055a28f705a3caf3a4f7ffafc1a373213fb1bb6
Shuo Chen <chenshuo@chenshuo.com>
2014-02-22 10:47:32 -0800

    finish simple: time and daytime, timeclient and discardclient

--- examples/simple/simple_allinone/main.go
+++ examples/simple/simple_allinone/main.go
@@ -7,6 +7,8 @@ import (
 func main() {
 	ch := make(chan bool)
 
+	// long connection
+
 	chargenServer := simple.NewChargenServer(":2019")
 	go chargenServer.Serve()
 
@@ -15,6 +17,14 @@ func main() {
 
 	echoServer := simple.NewEchoServer(":2007")
 	go echoServer.Serve()
+
+	// short connection
+
+	daytimeServer := simple.NewDaytimeServer(":2013")
+	go daytimeServer.Serve()
+
+	timeServer := simple.NewTimeServer(":2037")
+	go timeServer.Serve()
 
 	<-ch // wait forever
 }
